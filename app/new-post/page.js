8a01a37ae7a2404e9f065687d2cbfaf5886aa47c
cam lowndes <cameronlowndes@gmail.com>
2025-06-28 14:17:13 +0100

    done for today

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,40 +1,6 @@
+import { createPost } from "@/actions/post";
 import PostForm from "@/components/PostForm";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
-  async function createPost(prevState, formData) {
-    "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-
-    let errors = [];
-
-    if(!title || title.trim().length === 0) {
-      errors.push("Title is Required.")
-    }
-     if(!content || content.trim().length === 0) {
-       errors.push("Content is Required.")
-    }
-     if(!image || image.size === 0) {
-       errors.push("Image is Required.")
-    }
-
-    if (errors.length > 0) {
-      return {errors};
-    }
-
-    await storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    })
-
-    redirect('/feed');
-  }
-
- 
 return <PostForm action={createPost} />
 }
