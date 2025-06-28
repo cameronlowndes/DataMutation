f77d2a557a8dac652ce45202c1d7ad7f3ff04207
cam lowndes <cameronlowndes@gmail.com>
2025-06-28 13:42:42 +0100

    adding

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import Header from '@/components/header';
 import './globals.css';
 
 export const metadata = {
-  title: 'NextPosts',
+  title: 'JustDropped',
   description: 'Browse and share amazing posts.',
 };
 
